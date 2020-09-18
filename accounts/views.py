0fd2913930554ad24e68fbe32421b9523afde0df
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2020-09-18 11:52:27 +0600

    redering data to templete, showing all data to dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # https://docs.djangoproject.com/en/3.1/intro/tutorial01/#write-your-first-view
 from django.http import HttpResponse
+from .models import *
 
 
 # Create your views here.
@@ -9,11 +10,31 @@ from django.http import HttpResponse
 
 # https://docs.djangoproject.com/en/3.1/ref/request-response/
 def home(request):
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers =customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status="Delivered").count()
+    pending = orders.filter(status="Pending").count()
+
+    context = {
+        'orders': orders,
+        'custemers': customers,
+        "total_orders" : total_orders,
+        "delivered": delivered,
+        "total_customers": total_customers,
+        'pending': pending
+    }
+
+
     # https://docs.djangoproject.com/en/3.1/intro/tutorial03/#a-shortcut-render
-    return render(request, 'accounts/dashboard.html')
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    # https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+    return render(request, 'accounts/products.html', {'products': products})
 
 def customer(request):
     return render(request, 'accounts/customer.html')

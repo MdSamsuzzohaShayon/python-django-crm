c15e08c5a7cf682d0e43a5f272ab0e089ef289ec
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2020-09-19 17:50:10 +0600

    delete operation

--- accounts/views.py
+++ accounts/views.py
@@ -99,6 +99,19 @@ def updateOrder(request, pk):
 
 
 
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
+
+
+    context = {
+        "item" : order
+    }
+    return render(request, "accounts/delete.html", context)
+
+
 
 
 

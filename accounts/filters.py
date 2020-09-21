0d94b2cf0c6e6f0f930b3662ba15772c3d42eea0
MdSamsuzzohaShayon <mdshayon0@gmail.com>
2020-09-21 11:42:15 +0600

    date range filter, gater than or equal , less than or equal

--- accounts/filters.py
+++ accounts/filters.py
@@ -1,11 +1,16 @@
 # https://pypi.org/project/django-filter/
 
 import django_filters
+# https://django-filter.readthedocs.io/en/master/ref/filters.html#datefilter
+from django_filters import DateFilter
 
 from .models import *
 
 
 class OrderFilter (django_filters.FilterSet):
+    start_date = DateFilter(field_name="date_create", lookup_expr='gte')
+    end_date = DateFilter(field_name="date_create", lookup_expr='lte')
     class Meta:
         model = Order
         fields = '__all__'
+        exclude = ['customer', 'date_create']

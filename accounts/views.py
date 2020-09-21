7c4e4543616d2cb2614ece16535170b0d9077925
MdSamsuzzohaShayon <mdshayon0@gmail.com>
2020-09-21 16:40:31 +0600

    protecting route by validating is the user is authenticated or not

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,9 @@ from django.contrib import messages
 # https://docs.djangoproject.com/en/3.1/ref/contrib/auth/#module-django.contrib.auth.signals
 # https://docs.djangoproject.com/en/3.1/topics/auth/default/#how-to-log-a-user-in
 from django.contrib.auth import authenticate, login, logout
+# https://docs.djangoproject.com/en/2.2/_modules/django/contrib/auth/decorators/
+# https://docs.djangoproject.com/en/2.2/topics/auth/default/#django.contrib.auth.decorators.login_required
+from django.contrib.auth.decorators import login_required
 
 from .models import *
 from .forms import OrderForm, CreateUserForm
@@ -22,36 +25,43 @@ from .filters import OrderFilter
 
 # https://docs.djangoproject.com/en/1.8/_modules/django/contrib/auth/forms/
 def registerPage (request):
-    form = CreateUserForm()
-    if request.method == "POST":
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, "Account was created for " + user)
-            return redirect('login')
-    context ={
-        "form": form
-    }
-    return render(request, 'accounts/register.html', context)
+    # https://docs.djangoproject.com/en/3.1/ref/contrib/auth/#django.contrib.auth.models.User.is_authenticated
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == "POST":
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, "Account was created for " + user)
+                return redirect('login')
+        context ={
+            "form": form
+        }
+        return render(request, 'accounts/register.html', context)
 
 
 
 def loginPage (request):
-    if(request.method == 'POST'):
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if(request.method == 'POST'):
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request , user)
-            return redirect('home')
-        else:
-            messages.info(request, "Username or password is incorrect")
+            if user is not None:
+                login(request , user)
+                return redirect('home')
+            else:
+                messages.info(request, "Username or password is incorrect")
 
-    context ={}
-    return render(request, 'accounts/login.html', context)
+        context ={}
+        return render(request, 'accounts/login.html', context)
 
 
 
@@ -61,7 +71,9 @@ def logoutUser(request):
 
 
 
+# https://docs.djangoproject.com/en/2.2/topics/auth/default/#django.contrib.auth.decorators.login_required
 # https://docs.djangoproject.com/en/3.1/ref/request-response/
+@login_required(login_url='login')
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -87,7 +99,7 @@ def home(request):
 
 
 
-
+@login_required(login_url='login')
 def products(request):
     products = Product.objects.all()
     # https://docs.python.org/3/tutorial/datastructures.html#dictionaries
@@ -99,7 +111,7 @@ def products(request):
 
 
 
-
+@login_required(login_url='login')
 def customer(request, pk_test):
     customer = Customer.objects.get(id=pk_test)
     orders = customer.order_set.all()
@@ -125,7 +137,7 @@ def customer(request, pk_test):
 
 
 
-
+@login_required(login_url='login')
 def createOrder(request, pk):
     # https://docs.djangoproject.com/en/3.1/topics/forms/modelforms/#inline-formsets
     OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10)
@@ -147,7 +159,7 @@ def createOrder(request, pk):
 
 
 
-
+@login_required(login_url='login')
 def updateOrder(request, pk):
     order = Order.objects.get(id=pk)
     form = OrderForm(instance=order)
@@ -164,6 +176,7 @@ def updateOrder(request, pk):
 
 
 
+@login_required(login_url='login')
 def deleteOrder(request, pk):
     order = Order.objects.get(id=pk)
     if request.method == "POST":
